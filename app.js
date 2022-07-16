2096daaf2047dde678622a067ef4aa41c25a1058
Leila Alves Evangelista <leila.ae@gmail.com>
2022-07-16 21:50:03 +0000

    retirada do alert

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ function somar() {
     let numeroB = numB.value
 
     if (!numeroA || !numeroB || numeroA < 0 || numeroB < 0) {
-
-        alert(res.value) = "Preencha os campos para somar"
         return
     } else {
         res.value = parseInt(numeroA) + parseInt(numeroB)
